ccd41010826522ca4c2d692c8cfe4303a2400894
Ilham Novriadi <ilhamnovriadi@Ilhams-MacBook-Pro.local>
2020-04-26 11:53:02 +0700

    Generate Serial Number

---
+++ GenerateSerialNumber.js
@@ -0,0 +1,34 @@
+function acakSerial(panjangSerial) {
+  let char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let serialNumber = "";
+  let jumlahSerial = 250;
+  let arraySerial = new Array();
+  //Mengacak Character
+  while (jumlahSerial >= arraySerial.length) {
+    for (i = 0; i < panjangSerial; i++) {
+      randomChar = Math.floor(Math.random() * char.length);
+      serialNumber += char.substring(randomChar, randomChar + 1);
+    }
+    //Memotong string dengan strip (-)
+    String.prototype.splice = function (idx1, idx2, idx3, rem, str) {
+      return (
+        this.slice(0, idx1) +
+        str +
+        this.slice(idx1, idx2) +
+        str +
+        this.slice(idx2, idx3) +
+        str +
+        this.slice(idx3 + Math.abs(rem))
+      );
+    };
+    temp = serialNumber.splice(4, 8, 12, 0, "-");
+    arraySerial.push(temp);
+    serialNumber = "";
+  }
+  //Menanmpilkan Array dengan index
+  arraySerial.forEach(function (item, index, array) {
+    console.log(index, item);
+  });
+}
+let panjangSerial = 16;
+acakSerial(panjangSerial);
